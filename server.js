866b9b8919da4cf907dee784ec8402a09f3cc637
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-06-26 21:48:35 +0530

    Add parser for request

--- server.js
+++ server.js
@@ -2,12 +2,32 @@ const net = require('net')
 
 const server = net.createServer()
 
+const requestObject = {}
 server.on('connection', (socket) => {
   const remoteAddress = `${socket.remoteAddress} : ${socket.remotePort}`
   console.log('New client connected on ', remoteAddress)
 
   socket.on('data', (data) => {
-    console.log(data.toString())
+    const request = data.toString().split(/\r\n/)
+    console.log(request)
+    const requestLine = request[0]
+
+    const [method, requestUri, version] = requestLine.split(' ')
+    requestObject.method = method
+    requestObject.requestUri = requestUri
+    requestObject.version = version
+
+    const headers = {}
+    for(let i=1; i<request.length-2; i++){
+      const [headerType, headerValue] = request[i].split(':')
+      headers[headerType] = headerValue
+    }
+
+    requestObject.headers = headers
+
+    requestObject.body = request[request.length - 1]
+
+    console.log(requestObject)
     socket.write(`Server Reply : ${data}`)
   })
 
