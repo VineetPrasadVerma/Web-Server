6d8466b3d523c78469cea70a6a4c309aab93c956
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-07-06 15:50:38 +0530

    Made static handler general i.e. now it can read any static file

--- staticHandler.js
+++ staticHandler.js
@@ -1,34 +1,36 @@
 const fs = require('fs').promises
+const path = require('path')
 
 const contentType = {
-  js: 'application/js',
-  txt: 'text/plain',
-  html: 'text/html',
-  css: 'text/css',
-  json: 'application/json',
-  jpg: 'image/jpeg',
-  jpeg: 'image/jpeg',
-  png: 'image/png'
+  '.js': 'application/js',
+  '.txt': 'text/plain',
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.png': 'image/png'
 }
 
 const serveStaticFile = async (requestObj, respObj) => {
   let res = null
   try {
     if (requestObj.method === 'GET') {
-      if (requestObj.requestUri === '/') {
-        res =
+      if (requestObj.requestUri === '/') requestObj.requestUri = '/index.html'
+      res =
       'HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: Origin, Content-Type, Accept\r\n'
-        const date = new Date()
-        res += `Date: ${date.toUTCString()}\r\n`
-        res += `Content-Type: ${contentType.html}\r\n`
-        const body = await fs.readFile('./index.html')
-        res += `Content-Length: ${body.length}\r\n\r\n`
-        res += body
-      }
+      const date = new Date()
+      res += `Date: ${date.toUTCString()}\r\n`
+      res += `Content-Type: ${contentType[path.extname(requestObj.requestUri)]}\r\n`
+      const body = await fs.readFile(`./public/${requestObj.requestUri}`)
+      res += `Content-Length: ${body.length}\r\n\r\n`
+      res += body
+
       return res
     }
     return null
   } catch (err) {
+    console.log('Catch here', err)
     return null
   }
 }
